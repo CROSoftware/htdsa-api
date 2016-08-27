19b16dc10885ff5666b845e305637a6ad16c4442
Jordan Duty <djdduty@gmail.com>
2016-08-27 16:04:46 -0700

    Add append slashes feature

--- classes/Htdsa/API.php
+++ classes/Htdsa/API.php
@@ -21,7 +21,7 @@ class API{
 	 * @param $private_key
 	 * @param $public_key
 	 */
-	function __construct($endpoint, $identity, $private_key, $public_key, $debug = false)
+	function __construct($endpoint, $identity, $private_key, $public_key, $debug = false, $append_slash = false)
 	{
 		// remove trailing slashes in endpoints
 		if(substr($endpoint, -1) == '/'){
@@ -33,6 +33,7 @@ class API{
 		$this->private_key = $private_key;
 		$this->public_key = $public_key;
 		$this->debug = $debug;
+		$this->append_slash = $append_slash;
 	}
 
 	// subclass the API endpoint
@@ -40,6 +41,7 @@ class API{
 	{
 		// append endpoint and return new API object
 		$this->endpoint .= '/'.$name;
+		$this->endpoint .= ($this->append_slash == true ? '/' : '');
 
 		return new API($this->endpoint, $this->identity, $this->private_key, $this->public_key, $this->debug);
 	}
@@ -55,7 +57,7 @@ class API{
 		// remove array wrapping from __call() method
 		$args = $args[0];
 
-		$response = $this->net->post($this->endpoint.'/'.$name, $args);
+		$response = $this->net->post($this->endpoint.'/'.$name.($this->append_slash == true ? '/' : ''), $args);
 
 		if($response === false)
 		{
